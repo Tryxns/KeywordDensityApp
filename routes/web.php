bad3888f9682498439451b13863c55523594cc23
Fabian Jevon <Shar_evonz@hotmail.com>
2024-12-13 20:01:49 +0700

    update routing & navbar menu

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,5 @@ use App\Http\Controllers\ToolController;
 
 
 Route::post('/tool/calculate-and-get-density', [ToolController::class, 'CalculateAndGetDensity']);
-Route::get('/tool', [ToolController::class, 'index'])->name('KDTool');
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ToolController::class, 'index'])->name('KDTool');
 
